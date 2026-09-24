269ee687c22d4d9f8bcf8ceb55797d159d766f41
wagner <wagnerfilho321@gmail.com>
2026-09-24 10:36:21 -0300

    commit texto no sobre

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,25 @@
-export default function About(){
+export default function About() {
   return (
-    <div style={{padding:20}}>
+    <div style={{ padding: 20 }}>
       <h2>Sobre</h2>
-      <p>Aplicativo TaskFlow — exemplo de gerenciamento de tarefas.</p>
+
+      <p>
+        Nossa história nasceu da união de quatro visões complementares e do
+        objetivo compartilhado de transformar o mercado por meio da excelência,
+        inovação e compromisso com resultados. Fundada por Wagner, Marciedson,
+        Flávio e Paulo, nossa empresa combina a bagagem técnica, a inteligência
+        estratégica e a capacidade executiva de seus sócios para entregar
+        soluções de alto impacto.
+      </p>
+      <p>
+        
+      </p>
+      <p>
+        Acreditamos que a verdadeira força de um negócio reside na
+        complementaridade das pessoas que o constroem. É essa sinergia entre
+        quatro lideranças orientadas à excelência que define a nossa essência e
+        nos impulsiona a superar expectativas em cada entrega.
+      </p>
     </div>
-  )
+  );
 }
